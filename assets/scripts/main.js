92a4459049e9d4b1f24ed86b3bafdfcbc6d05449
amakhnia <amakhnia@ucsd.edu>
2021-02-05 12:07:17 -0800

    part1 solution updates

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,17 +5,17 @@ let volumeSlider = document.getElementById("volume-slider");
 let volumeNumber = document.getElementById("volume-number");
 let audioValue = document.getElementById("horn-sound");
 let volumeImage = document.getElementById("volume-image");
-let honkButton = document.getElementById("honk-button");
+let honkButton = document.getElementById("honk-btn");
 let radioAirHorn = document.getElementById("air-horn");
 let soundImage = document.getElementById("sound-image");
 let radioCarHorn = document.getElementById("car-horn");
 let radioPartyHorn = document.getElementById("party-horn");
 let formPartyHorn = document.getElementById("party-horn-form");
 
-radioAirHorn.addEventListener("click", updateAirHorn);
-radioCarHorn.addEventListener("click", updateCarHorn);
-radioPartyHorn.addEventListener("click", updatePartyHorn);
-formPartyHorn.addEventListener("click", function(v){
+radioAirHorn.addEventListener("change", updateAirHorn);
+radioCarHorn.addEventListener("change", updateCarHorn);
+radioPartyHorn.addEventListener("change", updatePartyHorn);
+honkButton.addEventListener("click", function(v){
     v.preventDefault();
     audioValue.play();
 });
@@ -35,20 +35,21 @@ function updateVolumeNumber(){
 }
 
 function updateVolumeImage() {
+    if (volumeNumber != 0){
+        honkButton.disabled = false;
+    }
+
     //volume-level-3: 67-100
     if (volumeNumber.value >= 67){
         volumeImage.src = "./assets/media/icons/volume-level-3.svg";
-        
     }
     //volume-level-2: 34-66
     else if (volumeNumber.value >= 34 && volumeNumber.value <= 66){
         volumeImage.src = "./assets/media/icons/volume-level-2.svg";
-        
     }
     //volume-level-1: 1-33
     else if (volumeNumber.value >= 1 && volumeNumber.value <= 33){
         volumeImage.src = "./assets/media/icons/volume-level-1.svg";
-        
     }
     //volume-level-0: 0
     else (volumeNumber.value == 0){
@@ -63,13 +64,13 @@ function updateAirHorn(){
     audioValue.scr = "./assets/media/audio/air-horn.mp3";
 }
 
-//radioCarHorn.addEventListener("click", updateCarHorn);
+//radioCarHorn.addEventListener("change", updateCarHorn);
 function updateCarHorn(){
     soundImage.scr = "./assets/media/images/car.svg";
     audioValue.scr = "./assets/media/audio/car-horn.mp3";
 }
 
-//radioPartyHorn.addEventListener("click", updatePartyHorn);
+//radioPartyHorn.addEventListener("change", updatePartyHorn);
 function updatePartyHorn(){
     soundImage.scr = "./assets/media/images/party-horn.svg";
     audioValue.scr = "./assets/media/audio/party-horn.mp3";
