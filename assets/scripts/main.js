d2afdb418632f13f1c0cab1c5b27ceff008dd233
amakhnia <57231301+amakhnia@users.noreply.github.com>
2021-02-05 19:47:42 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,9 +11,6 @@ let radioCarHorn = document.getElementById('radio-car-horn');
 let radioPartyHorn = document.getElementById('radio-party-horn');
 let formPartyHorn = document.getElementById('party-horn-form');
 
-
-document.getElementById('volume-number').addEventListener("input", updateVolumeSlider);
-
 radioAirHorn.addEventListener("click", updateAirHorn);
 radioCarHorn.addEventListener("click", updateCarHorn);
 radioPartyHorn.addEventListener("click", updatePartyHorn);
@@ -24,37 +21,37 @@ honkButton.addEventListener("click", function(v){
 
 document.getElementById('volume-slider').addEventListener("input", updateVolumeNumber);
 function updateVolumeSlider(){
-    volumeSlider.value = volumeNumber.value;
+    document.getElementById('volume-slider').value = document.getElementById('volume-number').value;
     updateVolumeImage();
-    audioValue.volume = volumeSlider.value / 100;
+    audioValue.volume = document.getElementById('volume-slider').value / 100;
 }
 
 document.getElementById('volume-number').addEventListener("input", updateVolumeSlider);
 function updateVolumeNumber(){
-    volumeNumber.value = volumeSlider.value;
+    document.getElementById('volume-number').value = document.getElementById('volume-slider').value;
     updateVolumeImage();
-    audioValue.volume = volumeNumber.value / 100;
+    audioValue.volume = document.getElementById('volume-number').value / 100;
 }
 
 function updateVolumeImage() {
 
     //volume-level-3: 67-100
-    if (volumeNumber.value >= 67){
+    if (document.getElementById('volume-number').value >= 67){
         volumeImage.src = "./assets/media/icons/volume-level-3.svg";
         honkButton.disabled = false;
     }
     //volume-level-2: 34-66
-    else if (volumeNumber.value >= 34 && volumeNumber.value <= 66){
+    else if (document.getElementById('volume-number').value >= 34 && document.getElementById('volume-number').value <= 66){
         volumeImage.src = "./assets/media/icons/volume-level-2.svg";
         honkButton.disabled = false;
     }
     //volume-level-1: 1-33
-    else if (volumeNumber.value >= 1 && volumeNumber.value <= 33){
+    else if (document.getElementById('volume-number').value >= 1 && document.getElementById('volume-number').value <= 33){
         volumeImage.src = "./assets/media/icons/volume-level-1.svg";
         honkButton.disabled = false;
     }
     //volume-level-0: 0
-    else if (volumeNumber.value == 0){
+    else if (document.getElementById('volume-number').value == 0){
         volumeImage.src = "./assets/media/icons/volume-level-0.svg";
         honkButton.disabled = true;
     }
